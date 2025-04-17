884fd5bc786d6d038de9a59fb1ff70d16598456e
Nightfury1010 <roshanpandey72106@gmail.com>
2025-04-17 21:20:16 +0545

    added a product page

---
+++ products/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the products index.")
+
+def new(request):
+    return HttpResponse("new products")
+
